1757862da5bfa481780ecabd015decbc2e329428
szumielxd <43210079+szumielxd@users.noreply.github.com>
2025-06-04 17:41:00 +0200

    Made reflections compatible with bukkit 1.20+

--- src/main/java/com/github/tukenuke/tuske/nms/ReflectionNMS.java
+++ src/main/java/com/github/tukenuke/tuske/nms/ReflectionNMS.java
@@ -1,6 +1,8 @@
 package com.github.tukenuke.tuske.nms;
 
 import java.lang.reflect.Constructor;
+import java.lang.reflect.Method;
+import java.util.Objects;
 import java.util.logging.Level;
 
 import com.github.tukenuke.tuske.util.ReflectionUtils;
@@ -15,10 +17,28 @@ import org.bukkit.inventory.ItemStack;
 import com.mojang.authlib.GameProfile;
 
 import com.github.tukenuke.tuske.TuSKe;
+import org.jetbrains.annotations.NotNull;
 
 public class ReflectionNMS implements NMS {
 
-	private String version = ReflectionUtils.packageVersion;
+
+	private final @NotNull Class<?> CRAFTITEM_CLASS = Objects.requireNonNull(ReflectionUtils.getCraftClass("inventory.CraftItemStack"));
+	private final @NotNull Method CRAFTITEM_ASNMSCOPY_METHOD = Objects.requireNonNull(ReflectionUtils.getMethod(CRAFTITEM_CLASS,"asNMSCopy", ItemStack.class));
+	private final @NotNull Class<?> NMSITEM_CLASS = Objects.requireNonNull(CRAFTITEM_ASNMSCOPY_METHOD.getReturnType());
+
+	private final @NotNull Class<?> CRAFTHUMAN_CLASS = Objects.requireNonNull(ReflectionUtils.getCraftClass("entity.CraftHumanEntity"));
+	private final @NotNull Method CRAFTHUMAN_GETHANDLE_METHOD = Objects.requireNonNull(ReflectionUtils.getMethod(CRAFTHUMAN_CLASS, "getHandle"));
+	private final @NotNull Class<?> NMSHUMAN_CLASS = Objects.requireNonNull(CRAFTHUMAN_GETHANDLE_METHOD.getReturnType());
+	private final @NotNull Method NMSHUMAN_DROP_METHOD = Objects.requireNonNull(ReflectionUtils.getMethod(NMSHUMAN_CLASS, "getHandle", NMSITEM_CLASS, boolean.class));
+
+	private final @NotNull Class<?> CRAFTPLAYER_CLASS = Objects.requireNonNull(ReflectionUtils.getCraftClass("entity.CraftPlayer"));
+	private final @NotNull Method CRAFTPLAYER_GETHANDLE_METHOD = Objects.requireNonNull(ReflectionUtils.getMethod(CRAFTPLAYER_CLASS, "getHandle"));
+	private final @NotNull Class<?> NMSPLAYER_CLASS = Objects.requireNonNull(CRAFTPLAYER_GETHANDLE_METHOD.getReturnType());
+
+	private final @NotNull Class<?> CRAFTSERVER_CLASS = Objects.requireNonNull(ReflectionUtils.getCraftClass("CraftServer"));
+	private final @NotNull Method CRAFTSERVER_GETSERVER_METHOD = Objects.requireNonNull(ReflectionUtils.getMethod(CRAFTSERVER_CLASS, "getServer"));
+
+
 	public ReflectionNMS(){
 	}
 
@@ -26,24 +46,24 @@ public class ReflectionNMS implements NMS {
 	public Player getToPlayer(OfflinePlayer p) {
 		if (!p.isOnline() && p.hasPlayedBefore()){
 			try {
-				Object sv = Bukkit.getServer().getClass().getDeclaredMethod("getServer").invoke(Bukkit.getServer());
-				Class<?> entityPlayerClz = Class.forName("net.minecraft.server.v" + version + ".EntityPlayer");
+				throw new UnsupportedOperationException("No more easy NMS");
+				/*Object sv = CRAFTSERVER_GETSERVER_METHOD.invoke(Bukkit.getServer());
 				Class<?> nms = Class.forName("net.minecraft.server.v"+version+".MinecraftServer");
 				Object worldServer = nms.getDeclaredMethod("getWorldServer", int.class).invoke(sv, 0);
 				Object playerInteractManager = Class.forName("net.minecraft.server.v" + version + ".PlayerInteractManager").getConstructors()[0].newInstance(worldServer);
 				GameProfile profile = new GameProfile(p.getUniqueId(), p.getName());
-				Constructor<?> constructor = entityPlayerClz.getConstructors()[0];
+				Constructor<?> constructor = NMSPLAYER_CLASS.getConstructors()[0];
 				Object newPlayer = constructor.newInstance(sv, worldServer, profile, playerInteractManager);
 				Player player = (Player) newPlayer.getClass().getDeclaredMethod("getBukkitEntity").invoke(newPlayer);
 				if (player != null){
 					player.loadData();
 					return player;
-				}
+				}*/
 			} catch (Exception e){
 				TuSKe.log(Level.WARNING,
 					"An error occured with expression to get player data. It is because your server version isn't supported yet.",
 					"So, report it somewhere, in Spigot or GitHub, to the developer with following details:",
-					"Running version: v" + version,
+					"Running version: v" + Bukkit.getVersion(),
 					"Error details:");
 				e.printStackTrace();
 			}
@@ -56,17 +76,14 @@ public class ReflectionNMS implements NMS {
 	public void makeDrop(Player p, ItemStack i) {
 		if (p != null && i != null){
 			try {
-				Class<?> craftItemClz = Class.forName("org.bukkit.craftbukkit.v"+version+".inventory.CraftItemStack");
-				Object nmsItem = craftItemClz.getDeclaredMethod("asNMSCopy", ItemStack.class).invoke(null, i);
-				Class<?> craftPlayerClz = Class.forName("org.bukkit.craftbukkit.v"+version+".entity.CraftPlayer");
-				Object entity = craftPlayerClz.getDeclaredMethod("getHandle").invoke(p);
-				Class.forName("net.minecraft.server.v"+version+".EntityHuman").getDeclaredMethod("drop", nmsItem.getClass(), boolean.class).invoke(entity, nmsItem, true);
-	
+				Object item = CRAFTITEM_ASNMSCOPY_METHOD.invoke(null, i);
+				Object entity = CRAFTPLAYER_GETHANDLE_METHOD.invoke(p);
+				NMSHUMAN_DROP_METHOD.invoke(entity, item, true);
 			} catch (Exception e){
 				TuSKe.log(Level.WARNING,
 					"An error occured with effect to force a player to drop a item. It is because your server version isn't supported yet.",
 					"So, report it somewhere, in Spigot or GitHub, to the developer with following details:",
-					"Running version: v" + version,
+					"Running version: v" + Bukkit.getVersion(),
 					"Error details:");
 				e.printStackTrace();
 				
